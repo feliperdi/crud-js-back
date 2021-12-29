0c9a870afb1662f89d00bf88fa4fc6bda32343c6
feliphe <dias.rodrigues.feliphe@gmail.com>
2021-12-29 11:36:02 -0300

    cors trying again

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import express from 'express';
 import categoryRouter from './routes/categoryRoutes';
 import router from './routes/itemRoutes';
 import helmet from 'helmet';
+import cors from 'cors';
 
 env.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json())
 app.use('/',router);
 app.use('/',categoryRouter);
